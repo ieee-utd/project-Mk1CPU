7ecab477c5ca0b7d058e328b3212d5176409a07e
Tazik89 <62776807+Tazik89@users.noreply.github.com>
2021-04-18 11:12:38 -0500

    Add files via upload

---
+++ Cache_Testbench.v
@@ -0,0 +1,77 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/14/2021 10:14:02 PM
+// Design Name: 
+// Module Name: Cache_Testbench
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Cache_Testbench;
+
+
+reg clk; 
+reg [1023:0] dataIn;
+reg control;
+reg [31:0] addr;
+reg [7:0] progInput;
+wire [7:0] dataOut;
+wire [1023:0] memOut;
+wire hit;
+
+Cache_Source test_Cache_Source (
+.clk(clk),
+.dataIn(dataIn),
+.control(control),
+.addr(addr),
+.progInput(progInput)
+);
+    initial 
+    begin
+        #200;
+        assign clk = 0; 
+        assign addr =4294967295'h00000000;
+        assign control = 1;
+        assign dataIn = 3'h01;
+        assign progInput = 2'h00;
+        #20 assign clk = 1; 
+        #20 assign clk = 0; 
+        assign addr =4294967295'h00000008;
+        assign control = 1;
+        assign dataIn = 3'h02;
+        assign progInput = 2'h00;
+        #20 assign clk = 1;
+        #20 assign clk = 0;  
+        assign addr = 4294967295'h000003FF;
+        assign control = 1;
+        assign dataIn = 3'h03;
+        assign progInput = 2'h00;
+        #20 assign clk = 1;
+        #20 assign clk = 0; 
+        assign addr = 4294967295'h00000400;
+        assign control = 1;
+        assign dataIn = 3'h04;
+        assign progInput = 2'h00;
+        #20 assign clk = 1;
+         #20 assign clk = 0; 
+        assign addr = 4294967295'h00001400;
+        assign control = 1;
+        assign dataIn = 3'h05;
+        assign progInput = 2'h00;
+        #20 assign clk = 1;
+        $finish;       
+end
+endmodule
---
+++ Source.v
@@ -0,0 +1,84 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/28/2021 05:25:40 PM
+// Design Name: 
+// Module Name: Cache_Source
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+/*  char - 1 byte
+    int - 4 byte
+    long int - 8 bytes 
+    double - 8 bytes     */   
+    
+
+module Cache_Source( 
+input clk, 
+input [1023:0] dataIn,
+input control,
+input [31:0] addr,
+input [7:0] progInput,
+output [7:0] dataOut,
+output  [1023:0] memOut,
+output hit
+ );
+ 
+parameter BLOCK_SIZE = 1024; 
+parameter NUM_BLOCKS = 4; 
+reg [19:0] tag;
+reg [1:0] index;
+reg [9:0] offset;
+reg [1023:0] RegisterFile1 [3:0];
+reg [20:0] tagTable [3:0]; // first bit is going to be valid bit, the rest is tag
+reg offsetInteger;
+reg [7:0] dataOut1; 
+reg [1023:0] memOut1; 
+reg temp = 1; 
+assign hit = 1;
+
+reg [31:0] newAddr; 
+always @ (posedge clk) 
+begin
+    tag <= addr [31:12];
+    index <= addr [ 11: 10];
+    offset <= addr [ 9: 0] ; 
+    offsetInteger <= offset [ 9:0];
+
+    if ( !(( tagTable [index][19:0] == tag [19:0] ) && (tagTable [index][20] == 1 )) ) 
+    begin
+          temp <= 0;
+          if (tagTable [index][20] == 1 )
+          begin
+               newAddr <= tagTable [index][19:0] << 11 + ( index << 8 );
+               memOut1 <= RegisterFile1 [index][1023:0]; 
+          end 
+          tagTable[index][20:0] <=  20'b10000000000000000000  +  tag[19:0];
+          RegisterFile1[index][1023:0] <= dataIn [ 1023:0 ];
+    end
+    if ( control )    
+    begin
+               dataOut1 <= RegisterFile1[index][offsetInteger +: 8]; 
+    end 
+    else
+    begin 
+               RegisterFile1[index] [offsetInteger +: 8] <= progInput [7:0];
+    end
+end
+assign dataOut = dataOut1 [7:0]; 
+assign memOut = memOut1[1023:0]; 
+
+    
+
+endmodule
